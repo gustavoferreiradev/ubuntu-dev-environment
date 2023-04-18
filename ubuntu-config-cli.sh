80925f7c87495ac0f3a97afa980a12604920d8d6
Gustavo Ferreira da Silva <gustavofersilva@gmail.com>
2023-04-17 22:00:02 -0300

    feat: Adicionado Neofetch

--- ubuntu-config-cli.sh
+++ ubuntu-config-cli.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt full-upgrade
-sudo apt install linux-headers-generic build-essential dkms git
+sudo apt install linux-headers-generic build-essential dkms git wget curl -y
 git clone https://github.com/clnhub/rtl8192eu-linux.git
 
 sudo rmmod 8192eu
@@ -22,8 +22,49 @@ sudo apt install tmux
 sudo apt install tilix
 sudo snap remove firefox
 
-sudo add-apt-repository ppa:lyzardking/ubuntu-make
+sudo add-apt-repository ppa:lyzardking/ubuntu-make -y
 sudo apt update
-sudo apt install ubuntu-make
+sudo apt install ubuntu-make -y
 sudo umake web firefox-dev
 
+sudo apt update
+sudo apt install \
+    ca-certificates \
+    curl \
+    gnupg -y
+    
+sudo install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt update
+sudo apt-get install google-chrome-stable
+
+
+
+echo \
+  "deb [arch="$(dpkg --print-architecture)" signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  "$(. /etc/os-release && echo "$VERSION_CODENAME")" stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+  
+  
+sudo apt update
+
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin -y
+
+sudo groupadd docker
+sudo usermod -aG docker $USER
+sudo chown "$USER":"$USER" /home/"$USER"/.docker -R
+sudo chmod g+rwx "$HOME/.docker" -R
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
+
+
+wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add –
+sudo add-apt-repository “deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main”
+sudo apt update
+sudo apt install code -y
+
+sudo apt install neofetch
